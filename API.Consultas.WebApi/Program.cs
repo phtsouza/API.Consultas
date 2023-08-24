88b182c637f9c8b40eabc0873818e6c73b0bf70b
Pedro Teixeira <pedroteixeira@frwk.com.br>
2023-08-24 14:23:05 -0300

    teste deploy

--- API.Consultas.WebApi/Program.cs
+++ API.Consultas.WebApi/Program.cs
@@ -1,3 +1,4 @@
+//using API.Consultas.Application;
 using API.Consultas.WebApi;
 
 public class Program
@@ -14,6 +15,8 @@ public class Program
         builder.Services.AddSwaggerGen();
 
         builder.Services.AddAutoMapper(typeof(WebApiMapperProfile));
+
+       // builder.Services.AddAplication();
 
         //builder.Services.AddDbRepositoryAdapter(
         //new DbRepositoryAdapterConfiguration()
